336690d7de9222d4a0ef8e50f8fd10dd81d84a79
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-02 16:58:39 -0800

    Fruit sprites

---
+++ Assets/Scripts/Bobber.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bobber : MonoBehaviour
+{
+    [SerializeField] float floatStrength;
+    [SerializeField] float bobDelay = 0;
+    Vector2 bobPosition;
+    float originalY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalY = transform.position.y;
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        bobPosition = transform.position;
+        bobPosition.y = originalY + (Mathf.Sin(Time.time) * floatStrength);
+        transform.position = bobPosition;
+    }
+  
+
+    IEnumerator Bob()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+            bobPosition = transform.position;
+            bobPosition.y = originalY + (Mathf.Sin(Time.time) * floatStrength);
+            transform.position = bobPosition;
+        }
+
+    }
+}
