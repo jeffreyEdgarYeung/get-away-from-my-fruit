b93262a1b6aeec335f251174bbe9f115492b7c98
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-02 16:04:22 -0800

    Spawning System

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnTime = 1f;
+    [SerializeField] float maxSpawnTime = 5f;
+    [SerializeField] Attacker enemyPrefab;
+
+    bool spawn = true;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            SpawnAttacker();
+        }
+        
+    }
+
+    private void SpawnAttacker()
+    {
+        var newEnemy = Instantiate
+        (
+            enemyPrefab,
+            transform.position,
+            Quaternion.identity
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
