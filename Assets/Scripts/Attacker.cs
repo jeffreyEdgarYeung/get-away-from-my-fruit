1dc14893c086d1bc14051a6574e029f4d8ad830f
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-03 14:19:29 -0800

    Animation event ms set

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    [Range(0f, 5f)][SerializeField] float walkSpeed = 1f;
+    [Range(0f, 5f)] float currSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +15,11 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime * walkSpeed);
+        transform.Translate(Vector2.left * Time.deltaTime * currSpeed);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currSpeed = speed;
     }
 }
