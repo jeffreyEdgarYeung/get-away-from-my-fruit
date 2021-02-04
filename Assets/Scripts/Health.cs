0830fa1520262768407b5743734f2a9f533c3e82
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-03 21:42:13 -0800

    Health and damage handling

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float maxHealth = 100f;
+
+    [Header("On Hit Effects")]
+    [SerializeField] Color hitTint;
+    [SerializeField] float hitFlashDuration = 0.1f;
+    [SerializeField] AudioClip[] hitSounds;
+    [SerializeField] float hitVolume = 1f;
+
+    // Cached Refs
+    SpriteRenderer spriteRenderer;
+    AudioSource audioSource;
+
+    float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandleHit(float damage)
+    {
+        PlayHitVFX();
+        StartCoroutine(HitFlash());
+        DealDamage(damage);        
+    }
+
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        if ( health <= 0) { Destroy(gameObject); }
+    }
+
+    private IEnumerator HitFlash()
+    {
+        spriteRenderer.color = hitTint;
+        yield return new WaitForSeconds(hitFlashDuration);
+        spriteRenderer.color = new Color(1,1,1,1);
+    }
+
+    private void PlayHitVFX()
+    {
+        AudioSource.PlayClipAtPoint(
+            hitSounds[Random.Range(0, hitSounds.Length)],
+            Camera.main.transform.position,
+            hitVolume
+        );
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float projectileSpeed = 1f;
+    [SerializeField] float projectileDamage = 30f;
     
     // Update is called once per frame
     void Update()
     {
         transform.Translate(new Vector3(projectileSpeed, 0, 0) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Attacker attacker = collision.gameObject.GetComponent<Attacker>();
+
+        if (attacker)
+        {
+            collision.gameObject.GetComponent<Health>().HandleHit(projectileDamage);
+            Destroy(gameObject);
+        }
     }
 }
