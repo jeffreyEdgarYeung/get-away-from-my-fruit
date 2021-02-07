85affccb44064d613b287072842764049c57eaf7
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-07 13:41:26 -0800

    Resource display

---
+++ Assets/Scripts/SunDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SunDisplay : MonoBehaviour
+{
+    // State
+    [SerializeField] int sun = 50;
+    [SerializeField] int maxSunLength = 4;
+
+    TextMeshProUGUI sunText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sunText = GetComponent<TextMeshProUGUI>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        sunText.text = GetSunString();
+    }
+
+    public void AddSun( int sunToAdd ){ 
+        sun += sunToAdd;
+        UpdateDisplay();
+    }
+
+    public void SpendSun( int sunToSpend ) { 
+        if ( sunToSpend > sun ) { return; }
+        sun -= sunToSpend;
+        UpdateDisplay();
+    }
+
+    private string GetSunString()
+    {
+        int numZeros = maxSunLength - sun.ToString().Length;
+
+        string zeros = "";
+
+        for (int i = 0; i < numZeros; i++)
+        {
+            zeros += "0";
+        }
+        return zeros + sun.ToString();
+    }
+}
