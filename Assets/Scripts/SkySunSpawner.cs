885acceac8c1c1566b97bb2de3ccb35a603f3b8a
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-08 14:27:15 -0800

    Sky sun spawn

---
+++ Assets/Scripts/SkySunSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkySunSpawner : MonoBehaviour
+{
+    [SerializeField] float xMin, xMax, yMin, yMax, coolDownMin, coolDownMax;
+    [SerializeField] Sun sunPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnSuns());
+    }
+
+    IEnumerator SpawnSuns()
+    {
+        while (true)
+        {
+            Vector3 spawnSpot = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0); 
+            yield return new WaitForSeconds(Random.Range(coolDownMin, coolDownMax));
+            Instantiate(sunPrefab, spawnSpot, Quaternion.identity);
+        }
+        
+    }
+}
