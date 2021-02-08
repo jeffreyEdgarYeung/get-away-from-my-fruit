e6f0e0d02d87ff6c25010dfe3cd2b7f90051e30e
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-08 12:19:37 -0800

    Sunflower Spawn

---
+++ Assets/Scripts/SunSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunSpawner : MonoBehaviour
+{
+    [SerializeField] Sun sunPrefab;
+    [SerializeField] float initDropWait = 7.75f;
+    [SerializeField] float coolDownMin = 0f;
+    [SerializeField] float coolDownMax = 10f;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(initDropWait);
+        SpawnSun();
+        StartCoroutine(SpawnSuns());
+        
+    }
+
+    IEnumerator SpawnSuns()
+    {
+        while( true )
+        {
+            yield return new WaitForSeconds(Random.Range(coolDownMin, coolDownMax));
+            SpawnSun();
+        }
+    }
+
+    private void SpawnSun()
+    {
+        Instantiate(sunPrefab, transform.position, Quaternion.identity);
+    }
+}
