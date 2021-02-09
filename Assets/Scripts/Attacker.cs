4b017d98657444b68e0d03b4683702012616b122
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-09 15:44:43 -0800

    Attacker state transition

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)] float currSpeed = 1f;
+    GameObject currTarget;
+
+    // Cached Refs
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,5 +25,11 @@ public class Attacker : MonoBehaviour
     public void SetMovementSpeed(float speed)
     {
         currSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        animator.SetBool("IsAttacking", true);
+        currTarget = target;
     }
 }
---
+++ Assets/Scripts/Lizard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lizard : MonoBehaviour
+{
+    private void OnTriggerEnter2D( Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(collision.gameObject);
+        }
+    }
+}
