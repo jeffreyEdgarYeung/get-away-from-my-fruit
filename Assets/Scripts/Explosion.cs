a103b90776b2e3606ab2bd0f392ff7952020dc41
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-11 15:39:53 -0800

    Potato Defender

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] int damage = 125;
+
+    [Header("SFX")]
+    [SerializeField] AudioClip explosionSFX;
+    [SerializeField] [Range(0f, 1f)] float explosionVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(explosionSFX, Camera.main.transform.position, explosionVolume);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Attacker>())
+        {
+            collision.GetComponent<Health>().HandleHit(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/PotatoMine.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoMine : MonoBehaviour
+{
+    [SerializeField] float armTime = 15f;
+
+    [SerializeField] GameObject explosionPrefab;
+
+    [Header("SFX")]
+    [SerializeField] AudioClip armingSFX;
+    [SerializeField] [Range(0f, 1f)] float armingVolume = 1f;
+    [SerializeField] AudioClip beepSFX;
+    [SerializeField] [Range(0f, 1f)] float beepVolume = 1f;
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(armTime);
+        GetComponent<Animator>().SetBool("IsArmed", true);
+        AudioSource.PlayClipAtPoint(armingSFX, Camera.main.transform.position, armingVolume);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (GetComponent<Animator>().GetBool("IsArmed") && collision.GetComponent<Attacker>())
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    public void Beep()
+    {
+        AudioSource.PlayClipAtPoint(beepSFX, Camera.main.transform.position, beepVolume);
+    }
+}
