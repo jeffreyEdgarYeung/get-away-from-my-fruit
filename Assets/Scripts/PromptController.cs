b9df31719e0b32b99a21a7db938c88e1a21c457a
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-17 11:05:27 -0800

    Prompt system

---
+++ Assets/Scripts/PromptController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PromptController : MonoBehaviour
+{
+    [SerializeField] string[] prompts;
+    [SerializeField] float initPromptDelay = 0f;
+    [SerializeField] float promptDuration = 5f;
+
+    [SerializeField] GameObject promptPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(initPromptDelay);
+        StartCoroutine(ShowPrompts());
+    }
+
+    IEnumerator ShowPrompts()
+    {
+        foreach(string prompt in prompts)
+        {
+            GameObject newPrompt = Instantiate
+            (
+                promptPrefab,
+                new Vector3(5f, 1.5f, 0),
+                Quaternion.identity
+            ) as GameObject;
+
+            newPrompt.GetComponent<Transform>().GetComponentInChildren<Text>().text = prompt;
+            newPrompt.GetComponent<Transform>().SetParent(transform);
+            yield return new WaitForSeconds(promptDuration);
+            Destroy(newPrompt);
+        }
+       
+    }
+}
