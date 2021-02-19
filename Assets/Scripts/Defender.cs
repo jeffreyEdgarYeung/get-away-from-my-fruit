32187c18653e63885499565679ec0866e200377f
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-18 18:48:49 -0800

    Shovel polish

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,19 +5,52 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] int sunCost = 100;
-    [SerializeField] Defender shovel;     // Used to recognize removal
+
+    [Header("Shovel Removal")]
+    [SerializeField] Defender shovel;        // Used to recognize removal
+    [SerializeField] GameObject fieldCursor;
+
+    GameObject thisFieldCursor;
 
     public int GetCost() { return sunCost; }
 
+    /*
+     * All following functions are for removal of defender through shovel
+     */
+
     private void OnMouseDown()
     {
+        if (GetComponent<Shovel>()) { return; }
         if(FindObjectOfType<DefenderSpawner>().GetDefender() == shovel)
         {
             Instantiate(shovel, transform.position, Quaternion.identity);
-        }
+            Destroy(gameObject);
+            Destroy(thisFieldCursor);
+            GameObject.Find("/Defender Bar/Shovel Button").GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 170);
+            FindObjectOfType<DefenderSpawner>().SetDefender(null);
+        } 
+    }
 
-        Destroy(gameObject);
-        GameObject.Find("/Defender Bar/Shovel Button").GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 170);
-        FindObjectOfType<DefenderSpawner>().SetDefender(null);
+    private void OnMouseEnter()
+    {
+        if (GetComponent<Shovel>()) { return; }
+
+        if (FindObjectOfType<DefenderSpawner>().GetDefender() != shovel) { return; }
+        thisFieldCursor = Instantiate
+        (
+            fieldCursor, 
+            transform.position, 
+            Quaternion.identity
+        ) as GameObject;
+    }
+
+    private void OnMouseExit() 
+    {
+        if (GetComponent<Shovel>()) { return; }
+
+        if (thisFieldCursor)
+        {
+            Destroy(thisFieldCursor);
+        }
     }
 }
