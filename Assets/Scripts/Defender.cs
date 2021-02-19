5a455032fe70efafcc188c5b097725bd98f5e9c2
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-18 18:30:41 -0800

    Shovel Functionality

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] int sunCost = 100;
+    [SerializeField] Defender shovel;     // Used to recognize removal
 
     public int GetCost() { return sunCost; }
+
+    private void OnMouseDown()
+    {
+        if(FindObjectOfType<DefenderSpawner>().GetDefender() == shovel)
+        {
+            Instantiate(shovel, transform.position, Quaternion.identity);
+        }
+
+        Destroy(gameObject);
+        GameObject.Find("/Defender Bar/Shovel Button").GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 170);
+        FindObjectOfType<DefenderSpawner>().SetDefender(null);
+    }
 }
---
+++ Assets/Scripts/Shovel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shovel : MonoBehaviour
+{
+    [SerializeField] [Range(0f, 1f)] float appearDuration = .5f;
+
+    [Header("SFX")]
+    [SerializeField] AudioClip sfx;
+    [SerializeField] [Range(0f, 1f)] float sfxVolume = 1f;
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        AudioSource.PlayClipAtPoint(sfx, Camera.main.transform.position, sfxVolume);
+        yield return new WaitForSeconds(appearDuration);
+        Destroy(gameObject);
+    }
+
+    /*
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("In there");
+        if (collision.GetComponent<Defender>())
+        {
+            collision.GetComponent<Health>().DealDamage(1000);
+        }
+    }
+    */
+}
